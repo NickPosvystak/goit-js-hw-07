27f3aecd2e23de42e0100dc4ccf3d315f1d541cc
NickPosvystak <126783947+NickPosvystak@users.noreply.github.com>
2023-07-22 21:01:02 +0200

    number 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+function renderGallery() {
+  const createElement = galleryItems
+    .map(
+      (item) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+    </a>
+    </li>`
+    )
+    .join("");
+  galleryList.innerHTML = createElement;
+}
+
+renderGallery();
+
+galleryList.addEventListener("click", onClick);
+
+function onClick(event) {
+  event.preventDefault();
+}
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionPosition: "bottom",
+  captionDelay: "250",
+  captionsData: "alt",
+});
